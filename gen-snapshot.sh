90b7a8a83088bdcae46477847e1c20b60b84c040
Daniel James <daniel@64studio.com>
2014-06-24 16:04:58 +0100

    BK-1014 Update snapshot generation script for Booktype 2.0

--- gen-snapshot.sh
+++ gen-snapshot.sh
@@ -1,12 +1,10 @@
 #/bin/sh
 # Script for generating nightly Booktype snapshot packages
-# Run from the directory containg the files checked out from git
+# Set GITPATH to the directory containg the Booktype source
 
-VERSION=1.6.0~$(date "+%Y%m%d")
+VERSION=2.0.0~$(date "+%Y%m%d")
+GITPATH=../booktype/
 BUILDDEST=/tmp/booktype-${VERSION}/
-
-git checkout devel
-git pull
 
 echo "cleaning up previous build..."
 
@@ -15,41 +13,47 @@ mkdir ${BUILDDEST}
 
 echo "copying files to temporary directory..."
 
+cp -a debian ${BUILDDEST} || exit
+
+cd ${GITPATH}
+git checkout 2.0
+git pull origin 2.0
 cp -a * ${BUILDDEST} || exit
 
 cd ${BUILDDEST} || exit
 
+# Build the documentation then move it out of the way
+
+python setup.py build_sphinx
+mv build/ docs/
+
 # Set the version of the snapshot package
 
-sed -i "1s:(1.6.0-1):(${VERSION}):g" debian/changelog
+sed -i "1s:(2.0.0-1):(${VERSION}):g" debian/changelog
 
-# Fixes for 1.6.0  #############
-
-# remove directories not needed by users
-rm -r tests
-rm -r tools
+# Fixes for 2.0.0  #############
 
 # moved to debian/copyright
 rm LICENSE.txt
-rm lib/booki/site_static/js/tiny_mce/classes/firebug/FIREBUG.LICENSE
-rm lib/booki/site_static/js/tiny_mce/license.txt
-rm lib/booki/site_static/js/jquery/AUTHORS.txt
-rm lib/booki/site_static/js/jquery/MIT-LICENSE.txt
-rm lib/booki/site_static/js/jquery/GPL-LICENSE.txt
-
-# remove Windows thumbnail files
-rm lib/booki/site_static/images/jquerybubblepopup-theme/all-black/Thumbs.db
-rm lib/booki/site_static/images/jquerybubblepopup-theme/all-black/ie/Thumbs.db
-rm lib/booki/site_static/images/jquerybubblepopup-theme/green/Thumbs.db
-rm lib/booki/site_static/images/jquerybubblepopup-theme/orange/Thumbs.db
-rm lib/booki/site_static/images/jquerybubblepopup-theme/violet/Thumbs.db
-rm lib/booki/site_static/images/jquerybubblepopup-theme/yellow/Thumbs.db
+rm lib/booktype/apps/edit/static/edit/js/aloha/plugins/common/align/LICENSE
+rm lib/booktype/apps/edit/static/edit/js/aloha/plugins/extra/flag-icons/img/flags/LICENSE
 
 # fix permissions
 chmod +x scripts/cron_reports.sh.original
 chmod +x scripts/cron_reports_weekly.sh.original
-chmod -x lib/booki/site_static/js/editor.js
-chmod -x lib/booki/site_static/images/feed-icon-14x14.png
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/it/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/pl/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/lib/format-plugin.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/booktype/styling/lib/styling-plugin.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/de/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/eo/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/fi/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/ru/i18n.js
+chmod -x lib/booktype/apps/edit/static/edit/js/aloha/plugins/oer/format/nls/fr/i18n.js
+
+# delete left-over gitignore file
+rm lib/booktype/apps/edit/static/edit/js/aloha/plugins/extra/draganddropfiles/demo/.gitignore
 
 #############################
 
