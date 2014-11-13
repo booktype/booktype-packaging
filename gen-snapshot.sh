35fbda049ad3f426e15d706fb9165a8b507d4b57
Daniel James <daniel@64studio.com>
2014-11-13 13:10:38 +0000

    Merge snapshot improvements from the Sourcefabric branch

--- gen-snapshot.sh
+++ gen-snapshot.sh
@@ -1,9 +1,12 @@
 #/bin/sh
 # Script for generating nightly Booktype snapshot packages
-# Set GITPATH to the directory containg the Booktype source
+# Set GITPATH to the directory containing the Booktype source
+# Set PKGPATH to the directory containing the packaging scripts
 
 VERSION=2.0.0~$(date "+%Y%m%d")
-GITPATH=../booktype/
+REVISION=1
+GITPATH=~/64studio/git/booktype/
+PKGPATH=~/64studio/git/booktype-packaging/
 BUILDDEST=/tmp/booktype-${VERSION}/
 ORIGBALL=/tmp/booktype_${VERSION}.orig.tar.gz
 
@@ -12,32 +15,30 @@ echo "cleaning up previous build..."
 rm -rf /tmp/booktype-*
 mkdir ${BUILDDEST}
 
-echo "copying files to temporary directory..."
-
-cp -a debian ${BUILDDEST} || exit
+echo "copying Booktype files to temporary directory..."
 
 cd ${GITPATH}
 git checkout 2.0
 git pull origin 2.0
 cp -a * ${BUILDDEST} || exit
-tar -cvzf ${ORIGBALL} ${GITPATH}
 
 cd ${BUILDDEST} || exit
 
-# Build the documentation then move it out of the way
+# Build the documentation then move it into place
 
 python setup.py build_sphinx
-mv build/ _build/
-rm -r docs/_build/
-mv _build/ docs/
+rm -r docs/_build
+mv build docs/_build
 
-# Strip the bundled Awesome font, it is a dependency
-rm -r lib/booktype/apps/core/static/core/css/font-awesome/css/
-rm -r lib/booktype/apps/core/static/core/css/font-awesome/font/
+echo "creating the source tarball..."
+tar -cvzf ${ORIGBALL} *
+
+echo "copying the packaging files..."
+cp -a ${PKGPATH}debian ${BUILDDEST} || exit
 
 # Set the version of the snapshot package
 
-sed -i "1s:(2.0.0-1):(${VERSION}):g" debian/changelog
+sed -i "1s:(2.0.0-1):(${VERSION}-${REVISION}):g" debian/changelog
 
 # Fixes for 2.0.0  #############
 
@@ -49,6 +50,8 @@ rm lib/booktype/apps/edit/static/edit/js/aloha/plugins/extra/flag-icons/img/flag
 # fix permissions
 chmod +x scripts/cron_reports.sh.original
 chmod +x scripts/cron_reports_weekly.sh.original
+chmod +x lib/booktype/skeleton/manage.py.original
+chmod +x lib/sputnik/redis-backuo.py
 
 #############################
 
@@ -57,7 +60,8 @@ echo "running the build..."
 debuild -d -uc -us $@ || exit
 
 # copy the new package to the public server
-# scp /tmp/booktype_${VERSION}_all.deb apt.sourcefabric.org:/var/www/apt/snapshots/
+# scp /tmp/booktype_${VERSION}-${REVISION}_all.deb apt.sourcefabric.org:/var/www/apt/snapshots/
 
-# copy the build log too
-# scp /tmp/booktype_${VERSION}_amd64.build apt.sourcefabric.org:/var/www/apt/snapshots/
+# copy the build log and changes too
+# scp /tmp/booktype_${VERSION}-${REVISION}_amd64.build apt.sourcefabric.org:/var/www/apt/snapshots/
+# scp /tmp/booktype_${VERSION}-${REVISION}_amd64.changes apt.sourcefabric.org:/var/www/apt/snapshots/
